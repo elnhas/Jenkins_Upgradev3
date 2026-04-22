7ec8c945ede8191e5b29019fb86188fea2be4c2b
elnhas <74294194+elnhas@users.noreply.github.com>
2026-04-22 17:04:22 +0200

    make comment

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -1,9 +1,9 @@
 package com.example;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.internal.matchers.GreaterThan;
-import org.mockito.internal.matchers.LessThan;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.mockito.internal.matchers.GreaterThan;
+//import org.mockito.internal.matchers.LessThan;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
